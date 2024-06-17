bd3c2aa3ff4fad9ed9cb705bd6472baa90feda89
Seeent <165791835+Seeent@users.noreply.github.com>
2024-06-17 10:27:13 +0000

    Mercadona.VicenteEsteban

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container mt-5">
+			<h1 className="text-center text-success">Lista de la compra</h1>
+			<ul className="row p-0">
+				
+				
+				<li className="border-0 p-0 ">
+					<input 
+					onChange={(e)=>setInputValue(e.target.value)}
+					value={inputValue}
+					onKeyDown={(e) => {
+						if(e.key === "Enter") {
+							setTodos(todos.concat(inputValue));
+							setInputValue("");
+						 }
+					}}
+					name="text"
+					placeholder="Me falta..." 
+					className="input "
+					type="text"/>
+				</li>
+				
+				{/* <input placeholder="Enter your text..." class="input" name="text" type="text"> */}
+
+				{todos.map((item,index) => (
+					<li>
+						{item}{" "}
+						<i className="icono mt-1 fa fa-solid fa-delete-left"
+						onClick={()=> 
+							setTodos(
+								todos.filter(
+									(todos,currentIndex)=>
+										index != currentIndex
+								)
+							)
+						}
+						></i>
+					</li>
+				))}
+				
+				<div className="p-0 text-white">{todos.length} elemento</div>
+				
+			</ul>
+			
+			
+			
 		</div>
+		
 	);
 };
 
+
 export default Home;
+
+
